c2879c6dc67dd6dd26983fccf694f05865a4cfc6
miyamae <miya.y.1201@gmail.com>
2019-12-22 20:46:39 +0900

    fix: next-offline

--- next.config.js
+++ next.config.js
@@ -4,6 +4,28 @@ const withOffline = require('next-offline');
 
 module.exports = withOffline(
   withCSS({
+    transformManifest: manifest => ['/'].concat(manifest),
+    generateInDevMode: true,
+    workboxOpts: {
+      swDest: 'static/service-worker.js',
+      runtimeCaching: [
+        {
+          urlPattern: /^https?.*/,
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'https-calls',
+            networkTimeoutSeconds: 15,
+            expiration: {
+              maxEntries: 150,
+              maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }
+      ]
+    },
     webpack: config => {
       config.resolve.alias = {
         ...config.resolve.alias,
